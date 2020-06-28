33b509da212259bf61d20b460ef925f174c25138
Robert Williams <robert@a3marketing.com>
2020-06-28 13:35:10 -0500

    created basic navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Navbar(props) {
+    return <nav className="navbar navbar-dark bg-dark">
+        <h3 className="navbar-header">Robert H Williams III</h3>
+    </nav>
+}
+
+
+export default Navbar;
