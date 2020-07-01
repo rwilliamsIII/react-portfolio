ec93dc941bf07870d51391f6f4944ae3f787529e
Robert Williams <robert@a3marketing.com>
2020-07-01 10:35:48 -0500

    finished languages component

--- src/components/Languages.js
+++ src/components/Languages.js
@@ -1,4 +1,13 @@
 import React from "react";
+import htmlPic from "../images/html-css-icon.png";
+import jsPic from "../images/javascript-logo.png";
+import jqueryPic from "../images/jquery-logo.png";
+import nodePic from "../images/nodejs-logo.png";
+import handlebarsPic from "../images/handlebars-logo.png";
+import passportPic from "../images/passport-icon.png";
+import mysqlPic from "../images/mysql-icon.png";
+import mongoPic from "../images/mongodb-icon.png";
+import awsPic from "../images/aws-logo.png";
 
 function Languages (props) {
     return (
@@ -6,63 +15,63 @@ function Languages (props) {
         <h1 className="technologies">Known Languages/Technologies:</h1>
           <div className="ui list">
             <div className="item">
-              <img className="ui avatar image" id="icon" src="assets/images/html-css-icon.png" alt=""/>
+              <img className="ui avatar image" id="icon" src={htmlPic} alt="html-css"/>
               <div className="content">
                 <p className="header" id="tech-header">HTML/CSS</p>
                 <div className="description"></div>
               </div>
             </div>
             <div className="item">
-              <img className="ui avatar image" id="icon" src="assets/images/javascript-logo.png" alt=""/>
+              <img className="ui avatar image" id="icon" src={jsPic} alt="javascript"/>
               <div className="content">
                 <p className="header" id="tech-header">JavaScript</p>
                 <div className="description"></div>
               </div>
             </div>
             <div className="item">
-              <img className="ui avatar image" id="icon" src="assets/images/jquery-logo.png" alt=""/>
+              <img className="ui avatar image" id="icon" src={jqueryPic} alt="jquery"/>
               <div className="content">
                 <p className="header" id="tech-header">JQuery</p>
                 <div className="description"></div>
               </div>
             </div>
             <div className="item">
-              <img className="ui avatar image" id="icon" src="assets/images/nodejs-logo.png" alt=""/>
+              <img className="ui avatar image" id="icon" src={nodePic} alt="nodeJS"/>
               <div className="content">
                 <p className="header" id="tech-header">NodeJS</p>
                 <div className="description"></div>
               </div>
             </div>
             <div className="item">
-              <img className="ui avatar image" id="icon" src="assets/images/handlebars-logo.png" alt=""/>
+              <img className="ui avatar image" id="icon" src={handlebarsPic} alt="handlebars"/>
               <div className="content">
                 <p className="header" id="tech-header">Handlebars</p>
                 <div className="description"></div>
               </div>
             </div>
             <div className="item">
-                <img className="ui avatar image" id="icon" src="assets/images/passport-icon.png" alt=""/>
+                <img className="ui avatar image" id="icon" src={passportPic} alt="passportJS"/>
                 <div className="content">
                   <p className="header" id="tech-header">Passport JS</p>
                   <div className="description"></div>
                 </div>
               </div>
               <div className="item">
-                <img className="ui avatar image" id="icon" src="assets/images/mysql-icon.png" alt=""/>
+                <img className="ui avatar image" id="icon" src={mysqlPic} alt="mysql"/>
                 <div className="content">
                   <p className="header" id="tech-header">MySQL</p>
                   <div className="description"></div>
                 </div>
               </div>
               <div className="item">
-                <img className="ui avatar image" id="icon" src="assets/images/mongodb-icon.png" alt=""/>
+                <img className="ui avatar image" id="icon" src={mongoPic} alt="mongoDB"/>
                 <div className="content">
                   <p className="header" id="tech-header">MongoDB</p>
                   <div className="description"></div>
                 </div>
               </div>
             <div className="item">
-                <img className="ui avatar image" id="icon" src="assets/images/aws-logo.png" alt=""/>
+                <img className="ui avatar image" id="icon" src={awsPic} alt="AWS"/>
                 <div className="content">
                   <p className="header" id="tech-header">Amazon Web Services</p>
                   <div className="description"></div>
