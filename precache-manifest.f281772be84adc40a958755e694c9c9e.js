80de75b09832cc1c63c51c4bfb12b1b6a81642da
Robert Williams <robert@a3marketing.com>
2020-07-02 13:16:06 -0500

    Updates

---
+++ precache-manifest.f281772be84adc40a958755e694c9c9e.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dc1e7835e003461ab9f2b4abd174562",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "0c918fd958d5ec493f4c",
+    "url": "/react-portfolio/static/css/main.01a05b28.chunk.css"
+  },
+  {
+    "revision": "a3e171b5cfeb0f27a358",
+    "url": "/react-portfolio/static/js/2.5299b02f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.5299b02f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c918fd958d5ec493f4c",
+    "url": "/react-portfolio/static/js/main.67a7babd.chunk.js"
+  },
+  {
+    "revision": "73a871a578ef5a2aeb49",
+    "url": "/react-portfolio/static/js/runtime-main.1d992053.js"
+  },
+  {
+    "revision": "33506df6492a9235f61891dcbdf5dbc3",
+    "url": "/react-portfolio/static/media/application-example.33506df6.gif"
+  },
+  {
+    "revision": "ecbf8568e5b5979bff9b1c817380e03b",
+    "url": "/react-portfolio/static/media/aws-logo.ecbf8568.png"
+  },
+  {
+    "revision": "34bfa20f8cecba62ea9fcd5fd301e5cd",
+    "url": "/react-portfolio/static/media/bio-pic.34bfa20f.jpg"
+  },
+  {
+    "revision": "02291b24fb56cb659caae9f6a7b7de43",
+    "url": "/react-portfolio/static/media/burger-example.02291b24.gif"
+  },
+  {
+    "revision": "5957e597aedb073030e46bb6c75cc30e",
+    "url": "/react-portfolio/static/media/directions-example.5957e597.gif"
+  },
+  {
+    "revision": "17957a1e2bc6c7e8dd07d085b6b7786f",
+    "url": "/react-portfolio/static/media/email-logo.17957a1e.png"
+  },
+  {
+    "revision": "6cda88e1b7909786f7e59c037056cddc",
+    "url": "/react-portfolio/static/media/employee-example.6cda88e1.gif"
+  },
+  {
+    "revision": "646702dcd5727a5505ce1a8e38fd5227",
+    "url": "/react-portfolio/static/media/ghc-example.646702dc.gif"
+  },
+  {
+    "revision": "2d0e3b763d84901425770e8f82a2a5ea",
+    "url": "/react-portfolio/static/media/handlebars-logo.2d0e3b76.png"
+  },
+  {
+    "revision": "27938b9fbbea19b2d2a1d358290c0887",
+    "url": "/react-portfolio/static/media/html-css-icon.27938b9f.png"
+  },
+  {
+    "revision": "d7b657e2f0728d9c034f0a9b87b1b727",
+    "url": "/react-portfolio/static/media/javascript-logo.d7b657e2.png"
+  },
+  {
+    "revision": "bfabb0d6088a59a5345cd9019927caa7",
+    "url": "/react-portfolio/static/media/jquery-logo.bfabb0d6.png"
+  },
+  {
+    "revision": "e5eee315a17de0d7f56117077eb71fa9",
+    "url": "/react-portfolio/static/media/mongodb-icon.e5eee315.png"
+  },
+  {
+    "revision": "20b30f18748a781a04c1430010c64190",
+    "url": "/react-portfolio/static/media/mysql-icon.20b30f18.png"
+  },
+  {
+    "revision": "63038b148e279dfea8be188beae210fd",
+    "url": "/react-portfolio/static/media/node-application-example.63038b14.gif"
+  },
+  {
+    "revision": "9100f2eb47cae949fb1f01853263103c",
+    "url": "/react-portfolio/static/media/nodejs-logo.9100f2eb.png"
+  },
+  {
+    "revision": "9e2e6dbb18be5b10cbff14d5b7d6d643",
+    "url": "/react-portfolio/static/media/passport-icon.9e2e6dbb.png"
+  }
+]);
